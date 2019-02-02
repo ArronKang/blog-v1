c8c80d80bd338122cba72684abb64237f4288c50
Garbage programmer <kangjunyun95@gmail.com>
2019-02-02 16:04:22 +0800

    分类

---
+++ src/main/java/com/kang/core/blog/blog/service/CatalogService.java
@@ -0,0 +1,35 @@
+package com.kang.core.blog.blog.service;
+
+import com.kang.core.blog.blog.domain.Catalog;
+import com.kang.core.blog.blog.domain.User;
+
+import java.util.List;
+
+public interface CatalogService {
+    /**
+     * 保存Catalog
+     * @param catalog
+     * @return
+     */
+    Catalog saveCatalog(Catalog catalog);
+
+    /**
+     * 删除Catalog
+     * @param id
+     * @return
+     */
+    void removeCatalog(Long id);
+
+    /**
+     * 根据id获取Catalog
+     * @param id
+     * @return
+     */
+    Catalog getCatalogById(Long id);
+
+    /**
+     * 获取Catalog列表
+     * @return
+     */
+    List<Catalog> listCatalogs(User user);
+}
